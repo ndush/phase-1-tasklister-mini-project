077e348ef07a13c8e3d6e62253512c508fdea891
damaris <mdamarisnduku@gmail.com>
2022-06-14 16:57:45 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    setToDo(e.target.task.value);
+    form.reset();
+  });
+})
+
+function setToDo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x";
+  li.textContent = `${todo}  `;
+
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+  
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
